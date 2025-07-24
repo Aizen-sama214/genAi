ac1249bd3b204c404b891affb0f19d3ebaed2f8f
Anirudh Kumar Yadiki <akyanirudh3@gmail.com>
2025-07-24 19:50:05 +0530

    built first langgraph workflow

---
+++ LangGraph/WorkFlows/basic_block.py
@@ -0,0 +1,23 @@
+import os
+from langchain_anthropic import ChatAnthropic
+from pydantic import BaseModel, Field
+from config import Config
+
+os.environ["ANTHROPIC_API_KEY"] = Config().anthropic_api_key
+
+# Define a tool
+def multiply(a: float, b: float) -> float:
+    return a * b
+    
+
+class SearchQuery(BaseModel):
+    search_query: str = Field(None, description="Query that is optimized web search.")
+    justification: str = Field(
+        None, description="Why this query is relevant to the user's request."
+    )
+
+class Llm():
+    def __init__(self, model_name: str = "claude-3-5-sonnet-latest"):
+        self.llm = ChatAnthropic(model=model_name)
+        self.llm_with_tools = self.llm.bind_tools([multiply])
+        self.structured_llm = self.llm_with_tools.with_structured_output(SearchQuery)
